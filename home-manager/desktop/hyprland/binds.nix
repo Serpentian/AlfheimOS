39c351322173584a5d765de51c941ba17e4935e2
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-01-07 19:34:24 +0300

    home: introduce resize binds

--- home-manager/desktop/hyprland/binds.nix
+++ home-manager/desktop/hyprland/binds.nix
@@ -84,4 +84,23 @@
       "$mod, mouse_up, workspace, r-1"
     ];
   };
+  wayland.windowManager.hyprland.extraConfig = ''
+    # will switch to a submap called resize
+    bind=$mod,R,submap,resize
+
+    # will start a submap called "resize"
+    submap=resize
+
+    # sets repeatable binds for resizing the active window
+    binde=,l,resizeactive,10 0
+    binde=,h,resizeactive,-10 0
+    binde=,k,resizeactive,0 -10
+    binde=,j,resizeactive,0 10
+
+    # use reset to go back to the global submap
+    bind=,escape,submap,reset 
+
+    # will reset the submap, meaning end the current one and return to the global one
+    submap=reset
+  '';
 }
