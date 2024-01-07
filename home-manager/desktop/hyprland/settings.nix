e23e129ca4a97eba307d87f3201cf96a7ac9e583
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-01-07 08:04:44 +0300

    home: autostart apps
    This commit makes Hyprland to autostart firefox and spotify_player
    on specifica workspaces.

--- home-manager/desktop/hyprland/settings.nix
+++ home-manager/desktop/hyprland/settings.nix
@@ -16,6 +16,8 @@
       "swww init; swww img ~/.dotfiles/non-nix/wallpapers/evening-sky.png"
       "input-remapper-control --command autoload"
       "spotifyd"
+      "[workspace 1 silent] firefox"
+      "[workspace 2 silent] kitty spotify_player"
     ];
     
     general = {
