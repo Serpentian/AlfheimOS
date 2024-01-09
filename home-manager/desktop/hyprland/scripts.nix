ad83162d7fdfd514045a3f1f49f10827a4a7c2d8
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-01-09 16:10:31 +0300

    home: fix music control script
    Now playerctl shows kdeconnect as a first input. Let's use the last one
    instead.

--- home-manager/desktop/hyprland/scripts.nix
+++ home-manager/desktop/hyprland/scripts.nix
@@ -62,7 +62,7 @@
       
       exec dbus-send                                                \
         --print-reply                                               \
-        --dest="org.mpris.MediaPlayer2.$(playerctl -l | head -n 1)" \
+        --dest="org.mpris.MediaPlayer2.$(playerctl -l | tail -n 1)" \
         /org/mpris/MediaPlayer2                                     \
         "org.mpris.MediaPlayer2.Player.$MEMBER"
     '')
