236dca82192cd247b6f868e57e9d7cf6975541cc
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-01-11 12:59:27 +0300

    vim: drop neovide
    Don't use it much.

--- non-nix/nvim/lua/config/options.lua
+++ non-nix/nvim/lua/config/options.lua
@@ -14,15 +14,3 @@ local options = {
 for k, v in pairs(options) do
     vim.opt[k] = v
 end
-
--- Helper function for transparency formatting
-local alpha = function()
-  return string.format("%x", math.floor(255 * vim.g.transparency or 0.8))
-end
-
-if vim.g.neovide then
-    vim.g.transparency = 0.8;
-    vim.g.neovide_transparency = 0.8
-    vim.opt.guifont = "RobotoMono Nerd Font:h12";
-    vim.g.neovide_background_color = "#11111b" .. alpha();
-end
--- non-nix/nvim/lua/plugins/colorscheme.lua
+++ non-nix/nvim/lua/plugins/colorscheme.lua
@@ -16,21 +16,9 @@ return {
                 treesitter = true,
                 notify = true,
                 neotree = true,
+                noice = true,
             }
         }
-        if not vim.g.neovide then
-            -- Disables setting the background color with terminal.
-            settings.transparent_background = true 
-        else
-            -- Otherwise we overwrite background color to be the same, as in terminal.
-            settings.highlight_overrides = {
-                mocha = function(mocha)
-                    return {
-                        Normal = {bg = "#11111b" },
-                    }
-                end,
-            }
-        end
         require("catppuccin").setup(settings)
         vim.cmd.colorscheme("catppuccin")
     end,
