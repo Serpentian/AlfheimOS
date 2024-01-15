620aa5a474f26f303844e38c05c8c99e8f89cecb
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-01-15 14:33:49 +0300

    vim: use tabs instead of spaces in c files

--- non-nix/nvim/lua/config/autocmds.lua
+++ non-nix/nvim/lua/config/autocmds.lua
@@ -1,3 +1,36 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = augroup("spaces"),
+    pattern = { "*" },
+    callback = function()
+        vim.opt.expandtab = true   -- convert tabs to spaces
+        vim.opt.tabstop = 4        -- insert 8 spaces for a tab
+        vim.opt.softtabstop = 4    -- fine tune the amount of spaces added
+        vim.opt.shiftwidth = 4     -- the amount of spaces in normal mode
+        vim.opt.smarttab = true    -- insert blanks on Tab key
+    end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = augroup("tabs"),
+    pattern = { "c" },
+    callback = function()
+        vim.opt.expandtab = false  -- don't convert tabs to spaces
+        vim.opt.tabstop = 8        -- insert 8 spaces for a tab
+        vim.opt.softtabstop = 0    -- fine tune the amount of spaces added
+        vim.opt.shiftwidth = 8     -- the amount of spaces in normal mode
+    end,
+})
+
+-- Executed automatically on save.
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    group = augroup("remove_trailing_whitespace"),
+    pattern = { "*" },
+    command = [[%s/\s\+$//e]],
+})
--- non-nix/nvim/lua/config/options.lua
+++ non-nix/nvim/lua/config/options.lua
@@ -3,12 +3,8 @@
 -- Add any additional options here
 
 local options = {
-    expandtab = true,                        -- convert tabs to spaces
-    tabstop = 4,                             -- insert 4 spaces for a tab
-    softtabstop = 4,                         -- fine tune the amount of spaces added
-    shiftwidth = 4,                          -- the amount of spaces in normal mode
-    showtabline = 4,                         -- always show tabs
     colorcolumn = "81",
+    autoindent = true,
 }
 
 for k, v in pairs(options) do
