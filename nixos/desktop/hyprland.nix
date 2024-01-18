7d581c3e5838c58fcb3dba1cf2af4a5480b9734a
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-01-18 14:38:16 +0300

    nixos: use Hyprland flake instead of nixpkgs
    Hope, it's newer.

--- nixos/desktop/hyprland.nix
+++ nixos/desktop/hyprland.nix
@@ -1,22 +1,17 @@
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
-  imports = 
-    [ 
+  imports =
+    [
       ./wayland.nix
       ./fonts.nix
     ];
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
-  };
 
   programs = {
     hyprland = {
       enable = true;
       xwayland.enable = true;
-      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     };
   };
 }
