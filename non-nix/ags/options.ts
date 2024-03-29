3e3fdfcda0275bc7fabe6faa24998724bc3d4b95
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-03-29 15:59:03 +0300

    ags: introduce variable number of spaces in overview
    If 0 is specified, then number of workspaces is automatically
    calculated.

--- non-nix/ags/options.ts
+++ non-nix/ags/options.ts
@@ -175,7 +175,7 @@ const options = mkOptions(OPTIONS, {
 
     overview: {
         scale: opt(9),
-        workspaces: opt(7),
+        workspaces: opt(0),
         monochromeIcon: opt(true),
     },
 
