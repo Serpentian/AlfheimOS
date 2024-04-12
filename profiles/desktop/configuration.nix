3dfa95e2679b95346efb9426079e04f75d63def2
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-04-12 03:34:54 +0300

    nixos: fix build

--- profiles/desktop/configuration.nix
+++ profiles/desktop/configuration.nix
@@ -11,7 +11,6 @@
         ../../system/security/virtualization/nemu
         ../../system/apps/cron.nix
         ../../system/gaming/steam.nix
-        ../../system/gaming/honkai.nix
         (if settings.enableVPN then ../../system/security/vpn.nix else null)
         (./. + "../../../system/wm"+("/" + builtins.elemAt settings.wm 0)+".nix")
         (./. + "../../../system/wm"+("/" + builtins.elemAt settings.wm 1)+".nix")
