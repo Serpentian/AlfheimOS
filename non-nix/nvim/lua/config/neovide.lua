a1706ae402c333fce609034e75b89a922fc09584
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-04-14 16:37:36 +0300

    vim: fix neovide font not found error

--- non-nix/nvim/lua/config/neovide.lua
+++ non-nix/nvim/lua/config/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
     vim.g.neovide_transparency = 0.8
     vim.g.neovide_refresh_rate = 120
-    vim.o.guifont = "JetBrains Mono:h12"
+    vim.o.guifont = "JetBrainsMono NF:h12"
     vim.g.neovide_padding_top = 15
 end
