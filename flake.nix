12281982ac934a31fb6370dbf3bf8b0f973b2245
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-04-17 21:48:19 +0300

    flake: separate settings from main file
    I'm going to use "git update-index --assume-unchanged" on another
    machine.

--- flake.nix
+++ flake.nix
@@ -2,40 +2,7 @@
     description = "Alfheim NixOs";
 
     outputs = { self, nixpkgs, home-manager, ... } @ inputs: let
-        settings = {
-            system = "x86_64-linux";
-            hostname = "alfheim"; # Hostname
-            username = "serpentian"; # Ssername
-            profile = "desktop"; # Select from profiles directory
-            timezone = "Europe/Moscow"; # Select timezone
-            locale = "en_US.UTF-8"; # Select locale
-            name = "Nikita Zheleztsov"; # Name (git config)
-            email = "n.zheleztsov@proton.me"; # Email (git config)
-            dotfilesDir = ".dotfiles"; # Absolute path of the local repo
-            theme = "catppuccin-mocha"; # Selected theme from themes directory (./themes/)
-            wm = ["hyprland"]; # Selected window manager or desktop environment;
-                                     # must select one in both ./user/wm/ and ./system/wm/
-                                     # Note, that first WM is incldued included into work profile
-                                     # second one includes both.
-
-            font = "JetBrains Mono"; # Selected font
-            fontPkg = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono"]; });
-            fontSize = 12; # Font size
-
-            icons = "Papirus";
-            iconsPkg = pkgs.papirus-icon-theme;
-
-            # Session variables.
-            editor = "nvim"; # Default editor
-            editorPkg = pkgs.neovim;
-            browser = "firefox"; # Default browser; must select one from ./user/app/browser/
-            browserPkg = pkgs.firefox;
-            term = "kitty"; # Default terminal command
-            termPkg = pkgs.kitty;
-
-            enableVPN = true; # Whether to include VPN configuration
-        };
-
+        settings = import (./. + "/settings.nix") {inherit pkgs;};
         pkgs = import nixpkgs {system = settings.system;};
     in {
         # NixOS configuration entrypoint.
---
+++ settings.nix
@@ -0,0 +1,34 @@
+{pkgs, ...}:
+{
+    system = "x86_64-linux";
+    hostname = "alfheim"; # Hostname
+    username = "serpentian"; # Ssername
+    profile = "desktop"; # Select from profiles directory
+    timezone = "Europe/Moscow"; # Select timezone
+    locale = "en_US.UTF-8"; # Select locale
+    name = "Nikita Zheleztsov"; # Name (git config)
+    email = "n.zheleztsov@proton.me"; # Email (git config)
+    dotfilesDir = ".dotfiles"; # Absolute path of the local repo
+    theme = "catppuccin-mocha"; # Selected theme from themes directory (./themes/)
+    wm = ["hyprland"]; # Selected window manager or desktop environment;
+                       # must select one in both ./user/wm/ and ./system/wm/
+                       # Note, that first WM is incldued included into work profile
+                       # second one includes both.
+
+    font = "JetBrains Mono"; # Selected font
+    fontPkg = (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono"]; });
+    fontSize = 12; # Font size
+
+    icons = "Papirus";
+    iconsPkg = pkgs.papirus-icon-theme;
+
+    # Session variables.
+    editor = "nvim"; # Default editor
+    editorPkg = pkgs.neovim;
+    browser = "firefox"; # Default browser; must select one from ./user/app/browser/
+    browserPkg = pkgs.firefox;
+    term = "kitty"; # Default terminal command
+    termPkg = pkgs.kitty;
+
+    enableVPN = true; # Whether to include VPN configuration
+}
