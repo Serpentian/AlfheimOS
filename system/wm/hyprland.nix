7d38768997782da07c6c8f62c5ae807254fe44be
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-04-22 21:20:48 +0300

    nixos: flake update

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -13,4 +13,8 @@
             package = inputs.hyprland.packages.${pkgs.system}.hyprland;
         };
     };
+    nix.settings = {
+        substituters = ["https://hyprland.cachix.org"];
+        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
 }
