9e6b91afc1e2fcaf007869a3d86572632fdf0f3b
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-05-12 13:22:53 +0300

    nixos: try to fix crackling sound

--- system/hardware/sound.nix
+++ system/hardware/sound.nix
@@ -11,4 +11,11 @@
     jack.enable = true;
   };
   hardware.pulseaudio.enable = false;
+  services.pipewire.extraConfig.pipewire = {
+      "10-clock-rate" = {
+          "context.properties" = {
+              "default.clock.allowed-rates" = [ 44100 48000 96000 ];
+          };
+      };
+  };
 }
