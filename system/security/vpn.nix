21d0a3bec0d28a75ebf0855abcb17932a65ac1fe
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-05-24 10:23:29 +0300

    nixos: install v2raya
    See you, space cowboy ... Wireguard was finally blocked by my IPS.

--- system/security/vpn.nix
+++ system/security/vpn.nix
@@ -2,12 +2,11 @@
 {
     networking.wg-quick.interfaces = {
         wg0 = {
-            autostart = true;
+            autostart = false;
             address = [ "10.200.200.3/32" ];
             privateKeyFile = "/etc/wireguard/private.key";
             dns = [ "10.200.200.1" ];
             mtu = 1450;
-
             peers = [{
                 publicKey = "0catAJY1uisr7cL8qu9t3Nwo6nlf6Go7rByyi0iWCC0=";
                 presharedKeyFile = "/etc/wireguard/preshared.key";
@@ -17,4 +16,6 @@
             }];
         };
     };
+
+    services.v2raya.enable = true;
 }
