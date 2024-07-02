c9ef2ef9fe776fe8348a742a9ede45fb2dada416
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-07-02 15:45:45 +0300

    nix: add hyprexpo
    Hope, it'll get window dragging soon

--- user/wm/hyprland.nix
+++ user/wm/hyprland.nix
@@ -8,11 +8,16 @@
         ./hyprland/scripts.nix
         ./hyprland/rules.nix
         ./hyprland/settings.nix
+        ./hyprland/plugins.nix
     ];
 
     wayland.windowManager.hyprland = {
         enable = true;
         package = pkgs.hyprland;
         systemd.enable = true;
+        plugins = [
+            # inputs.hyprland-plugins.packages.${pkgs.system}.hyprexpo
+            pkgs.hyprlandPlugins.hyprexpo
+        ];
     };
 }
---
+++ user/wm/hyprland/plugins.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, ... }:
+
+{
+  wayland.windowManager.hyprland.extraConfig = ''
+    bind=$mod,grave,hyprexpo:expo, toggle
+    plugin {
+        hyprexpo {
+            columns = 3
+            gap_size = 5
+            bg_col = rgb(111111)
+            workspace_method = center current # [center/first] [workspace] e.g. first 1 or center m+1
+
+            enable_gesture = true # laptop touchpad
+            gesture_fingers = 3  # 3 or 4
+            gesture_distance = 300 # how far is the "max"
+            gesture_positive = true # positive = swipe down. Negative = swipe up.
+        }
+    }
+  '';
+}
