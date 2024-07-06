7e1db950d6cf218f1c32486e7b13d07164a41e17
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-07-06 16:09:11 +0300

    work: add tlp

---
+++ system/apps/tlp.nix
@@ -0,0 +1,4 @@
+{ config, pkgs, ... }:
+{
+    services.tlp.enable = true;
+}
--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -8,7 +8,6 @@
 
     environment.systemPackages = with pkgs; [
         brightnessctl
-        pactl
     ];
 
     programs = {
