d5bfc1e3eff339696f1a9c378187de3bea1c9960
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-07-31 17:08:49 +0300

    graphic: trying to make StableDiffusion work

--- system/hardware/desktop/graphic.nix
+++ system/hardware/desktop/graphic.nix
@@ -1,8 +1,21 @@
 { config, pkgs, ... }:
 
 {
-    hardware.graphics.enable32Bit = true;
     environment.variables = {
         AMD_VULKAN_ICD = "RADV";
     };
+
+    hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = with pkgs; [
+            rocmPackages.clr
+            rocmPackages.rocminfo
+            rocmPackages.rocm-runtime
+        ];
+    };
+    # This is necesery because many programs hard-code the path to hip
+    systemd.tmpfiles.rules = [
+        "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+    ];
 }
