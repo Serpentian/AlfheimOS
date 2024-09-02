d9c13d0ee603fa922e0e30e574a68321bfdcc4e1
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-09-02 16:24:08 +0300

    nix: flake update

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -16,6 +16,7 @@
             enable = true;
             xwayland.enable = true;
             package = inputs.hyprland.packages.${pkgs.system}.default;
+            portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
         };
     };
 
