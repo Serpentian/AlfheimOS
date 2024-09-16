935b1f24f56a89b3cffd96defcb2e9409ef332ef
Nikita Zheleztsov <n.zheleztsov@proton.me>
2024-09-16 15:58:35 +0300

    neovim: fix lualine warnings

--- user/apps/neovim/plugins/ui/lualine.nix
+++ user/apps/neovim/plugins/ui/lualine.nix
@@ -1,33 +1,32 @@
 {
     programs.nixvim.plugins.lualine = {
         enable = true;
-        alwaysDivideMiddle = true;
-        globalstatus = true;
-        ignoreFocus = ["neo-tree"];
-        extensions = ["fzf"];
-        theme = "auto";
-        componentSeparators = {
-            left = "|";
-            right = "|";
-        };
-        sectionSeparators = {
-            left = "█"; # 
-            right = "█"; # 
-        };
-        sections = {
-            lualine_a = ["mode"];
-            lualine_b = [
-            {
-                name = "branch";
-                icon = "";
-            }
-            "diff"
-                "diagnostics"
-            ];
-            lualine_c = ["filename"];
-            lualine_x = ["filetype"];
-            lualine_y = ["progress"];
-            lualine_z = [''" " .. os.date("%R")''];
+        settings = {
+            theme = "auto";
+            extensions = ["fzf"];
+            always_divide_middle = true;
+            ignore_focus = ["neo-tree"];
+            globalstatus = true;
+            component_separators = {
+                left = "|";
+                right = "|";
+            };
+            section_separators = {
+                left = "█"; # 
+                right = "█"; # 
+            };
+            sections = {
+                lualine_a = ["mode"];
+                lualine_b = [
+                    "branch"
+                    "diff"
+                    "diagnostics"
+                ];
+                lualine_c = ["filename"];
+                lualine_x = ["filetype"];
+                lualine_y = ["progress"];
+                lualine_z = [''" " .. os.date("%R")''];
+            };
         };
     };
 }
