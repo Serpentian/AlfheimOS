19f85ebadbf88ac36586486cd092d40025777e3e
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-02-04 18:17:30 +0300

    neovim: change nvim-tree workdir on session load

--- user/apps/neovim/plugins/editor/session.nix
+++ user/apps/neovim/plugins/editor/session.nix
@@ -1,14 +1,23 @@
 {
     programs.nixvim.plugins.auto-session = {
         enable = true;
-        settings = {
-            use_git_branch = true;
-            auto_create.__raw = ''
-            function()
+        luaConfig.post = ''
+        require('auto-session').setup {
+            use_git_branch = true,
+            auto_create = function()
                 local cmd = 'git rev-parse --is-inside-work-tree'
                 return vim.fn.system(cmd) == 'true\n'
-            end
-            '';
-        };
+            end,
+            post_restore_cmds = {
+                function()
+                    -- Restore nvim-tree after a session is restored
+                    local nvim_tree_api = require('nvim-tree.api')
+                    nvim_tree_api.tree.open()
+                    nvim_tree_api.tree.change_root(vim.fn.getcwd())
+                    nvim_tree_api.tree.reload()
+                end
+            },
+        }
+        '';
     };
 }
