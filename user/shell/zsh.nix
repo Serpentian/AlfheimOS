b553728144a5834f93ad42fc4a29170966b45917
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-02-26 10:04:08 +0300

    shell: add mcfly

--- user/shell/zsh.nix
+++ user/shell/zsh.nix
@@ -1,56 +1,62 @@
 { settings, config, pkgs, ... }:
 
 {
-  imports = [
-      ./starship.nix
-  ];
+    imports = [
+        ./starship.nix
+    ];
 
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    enableCompletion = true;
-    syntaxHighlighting.enable = true;
-    shellAliases = {
-      ls = "exa --color=auto --icons";
-      l = "ls -l";
-      la = "ls -a";
-      lla = "ls -la";
-      lt = "ls --tree";
-      ".." = "cd ..";
-      "..." = "cd ../..";
-      "...." = "cd ../../../..";
-      "....." = "cd ../../../../..";
-      "......" = "cd ../../../../../..";
-      cat = "bat";
-      gs = "git status";
-      gd = "git diff";
-      gl = "git log";
-      ga = "git add";
-      gc = "git commit";
-      gca = "git commit -a";
-      vim = "nvim";
-      v = "vim";
-      mv = "mv -v";
-      cp = "rsync -avhW --no-compress --progress";
-      rm = "rm -rv";
-      w3md = "w3m https://lite.duckduckgo.com/lite/";
-      nix-tarantool = "nix develop ${settings.dotfilesDir}/shells/tarantool -c zsh";
-      nix-python = "nix develop ${settings.dotfilesDir}/shells/python -c zsh";
-      nix-lampray = "nix develop ${settings.dotfilesDir}/shells/lampray -c zsh";
-      nix-invoke = "nix develop ${settings.dotfilesDir}/shells/invoke -c zsh";
-      nix-comfy = "nix develop ${settings.dotfilesDir}/shells/comfy -c zsh";
-      neofetch = "neofetch --source ${settings.dotfilesDir}/user/apps/neofetch/snufkin.txt";
-      ytfzf = "ytfzf -T chafa";
-      nekoray = "xhost + local:; sudo nekoray";
-      mp = "ncmpcpp";
+    programs.fzf.enable = true;
+    programs.mcfly  = {
+        enable = true;
+        fzf.enable = true;
     };
-    initExtra = ''
-      set -o emacs
-    '';
-  };
 
-  home.packages = with pkgs; [
-    eza
-    bat
-  ];
+    programs.zsh = {
+        enable = true;
+        autosuggestion.enable = true;
+        enableCompletion = true;
+        syntaxHighlighting.enable = true;
+        shellAliases = {
+            ls = "exa --color=auto --icons";
+            l = "ls -l";
+            la = "ls -a";
+            lla = "ls -la";
+            lt = "ls --tree";
+            ".." = "cd ..";
+            "..." = "cd ../..";
+            "...." = "cd ../../../..";
+            "....." = "cd ../../../../..";
+            "......" = "cd ../../../../../..";
+            cat = "bat";
+            gs = "git status";
+            gd = "git diff";
+            gl = "git log";
+            ga = "git add";
+            gc = "git commit";
+            gca = "git commit -a";
+            vim = "nvim";
+            v = "vim";
+            mv = "mv -v";
+            cp = "rsync -avhW --no-compress --progress";
+            rm = "rm -rv";
+            w3md = "w3m https://lite.duckduckgo.com/lite/";
+            nix-tarantool = "nix develop ${settings.dotfilesDir}/shells/tarantool -c zsh";
+            nix-python = "nix develop ${settings.dotfilesDir}/shells/python -c zsh";
+            nix-lampray = "nix develop ${settings.dotfilesDir}/shells/lampray -c zsh";
+            nix-invoke = "nix develop ${settings.dotfilesDir}/shells/invoke -c zsh";
+            nix-comfy = "nix develop ${settings.dotfilesDir}/shells/comfy -c zsh";
+            neofetch = "neofetch --source ${settings.dotfilesDir}/user/apps/neofetch/snufkin.txt";
+            ytfzf = "ytfzf -T chafa";
+            nekoray = "xhost + local:; sudo nekoray";
+            mp = "ncmpcpp";
+        };
+        initExtra = ''
+            set -o emacs
+        '';
+    };
+
+    home.packages = with pkgs; [
+        eza
+        bat
+    ];
 }
