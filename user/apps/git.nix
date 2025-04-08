7dc40c3a47b1c23b2215b7a64f4eda23068ca8de
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-04-08 05:05:36 +0300

    git: add lazygit

--- user/apps/git.nix
+++ user/apps/git.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ...}:
 
 {
-  home.packages = [ pkgs.git ];
-  programs.git.enable = true;
-  programs.git.userName = "Nikita Zheleztsov";
-  programs.git.userEmail = "n.zheleztsov@proton.me";
-  programs.git.extraConfig = {
-    core.editor = "nvim";
-  };
+    programs.git = {
+        enable = true;
+        userName = "Nikita Zheleztsov";
+        userEmail = "n.zheleztsov@proton.me";
+        extraConfig.core.editor = "nvim";
+    };
+
+    programs.lazygit = {
+        enable = true;
+    };
 }
