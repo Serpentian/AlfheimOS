b16ed8533f0445218f41d55c1bf69dc16529d17b
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-04-22 12:06:17 +0300

    neovim: tweak neovide

--- user/apps/neovim/colorscheme/catppuccin.nix
+++ user/apps/neovim/colorscheme/catppuccin.nix
@@ -6,4 +6,20 @@
             transparent_background = true;
         };
     };
+
+    programs.nixvim.extraConfigLua = ''
+if vim.g.neovide then
+    require('catppuccin').setup({
+        transparent_background = false,
+        color_overrides = {
+            mocha = {
+                base = "#11111b",
+            },
+        },
+    })
+    -- setup must be called before loading
+    vim.cmd.colorscheme "catppuccin"
+end
+    '';
+
 }
--- user/apps/neovim/neovide.nix
+++ user/apps/neovim/neovide.nix
@@ -5,7 +5,9 @@ in {
     programs.nixvim.extraConfigLua = ''
 if vim.g.neovide then
     vim.g.neovide_refresh_rate = 120
-    vim.g.neovide_transparency = ${opacity}
+    vim.g.neovide_opacity = ${opacity}
+    vim.g.neovide_no_idle = true
+    vim.g.neovide_scroll_animation_far_lines = 9999
     vim.o.guifont = "${settings.font}:h${fontSize}"
     vim.g.neovide_padding_top = 15
 end
