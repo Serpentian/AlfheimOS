512a0dcaf27460449631c78787bb55f769d10cfd
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-04-29 15:23:42 +0300

    nvim: fix treesitter

--- user/apps/neovim/plugins/editor/treesitter.nix
+++ user/apps/neovim/plugins/editor/treesitter.nix
@@ -1,17 +1,24 @@
 { pkgs, ... }:
 {
+    home.packages = with pkgs; [
+        # Any C compiler is required for proper work of the treesitter.
+        gcc
+    ];
+
     programs.nixvim.plugins.treesitter = {
         enable = true;
         folding = false;
         nixvimInjections = true;
+        grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+            json
+            tlaplus
+            latex
+        ];
         settings = {
             indent.enable = true;
             highlight.enable = true;
             playground.enable = true;
             auto_install = true;
-            ensure_installed = [
-                "tlaplus"
-            ];
         };
     };
 }
