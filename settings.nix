db606711035d4e151a7e03611a07b83ccda4c09d
Nikita Zheleztsov <n.zheleztsov@proton.me>
2025-11-25 08:47:15 +0300

    settings: refactor ordering

--- settings.nix
+++ settings.nix
@@ -1,23 +1,27 @@
 {pkgs, inputs, ...}:
 rec {
+    # Basic configuration.
     system = "x86_64-linux";
+    profile = "desktop"; # Select from profiles directory
     hostname = "alfheim"; # Hostname
     username = "serpentian"; # Username
-    profile = "desktop"; # Select from profiles directory
     timezone = "Europe/Moscow"; # Select timezone
     locale = "en_US.UTF-8"; # Select locale
     name = "Nikita Zheleztsov"; # Name (git config)
     email = "n.zheleztsov@proton.me"; # Email (git config)
     dotfilesDir = "/home/${username}/.dotfiles"; # Absolute path of the repo;
 
-    theme = "catppuccin"; # Selected theme from themes directory (./themes/)
-    themeDetails = import (./. + "/themes/${theme}.nix") {inherit pkgs;};
-
+    # App configurations.
     shell = "zsh"; # See user/shells directory.
-
-    wms = ["hyprland" ]; # See user/wm/ and system/wm directories.
     editors = ["neovim"]; # See user/editors directory.
     browsers = ["zen-browser" "qutebrowser" ]; # See user/browsers directory.
     preferredEditor = "nvim"; # Session variable $TERM.
     preferredBrowser = "zen"; # Session variable $BROWSER.
+
+    # WM and theming.
+    wms = ["hyprland" ]; # See user/wm/ and system/wm directories.
+    theme = "catppuccin"; # Selected theme from themes directory (./themes/)
+
+    # Do not change these!
+    themeDetails = import (./. + "/themes/${theme}.nix") {inherit pkgs;};
 }
