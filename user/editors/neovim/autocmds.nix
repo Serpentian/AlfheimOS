abc392e34fce75c6b657bfa0bb4654a4d04832b2
Nikita Zheleztsov <n.zheleztsov@proton.me>
2026-04-23 19:26:01 +0300

    Revert "neovim: auto remove empty line at the end of the file"
    This reverts commit 63f5b4624861aacd5c1d1f7d18849a86c55ae4e1.

--- user/editors/neovim/autocmds.nix
+++ user/editors/neovim/autocmds.nix
@@ -3,7 +3,6 @@
         spaces = {};
         tabs = {};
         removeTrailingSpace = {};
-        removeTrailingEnter = {};
         panes = {};
     };
 
@@ -48,20 +47,6 @@
             function(ev)
                 save_cursor = vim.fn.getpos(".")
                 vim.cmd([[%s/\s\+$//e]])
-                vim.fn.setpos(".", save_cursor)
-            end
-            '';
-        };
-    }
-    {
-        group = "removeTrailingEnter";
-        event = ["BufWritePre"];
-        pattern = "*";
-        callback = {
-            __raw = ''
-            function(ev)
-                save_cursor = vim.fn.getpos(".")
-                vim.cmd([[%s/\_s*\%$//e]])
                 vim.fn.setpos(".", save_cursor)
             end
             '';
